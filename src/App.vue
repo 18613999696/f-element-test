d70c76f2b6e5426075c443db4dca89a4c3ccd1f9
fdzzt <18613999696@163.com>
2025-05-29 15:40:34 +0800

    feat: 还原

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -25,7 +25,7 @@ import HelloWorld from './components/HelloWorld.vue';
       >
     </a>
   </div>
-  <HelloWorld msg="vite+vue22" />
+  <HelloWorld msg="vite+vue" />
 </template>
 
 <style scoped>
